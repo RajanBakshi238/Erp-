548354d50d4296e78009b877b5bfb0adbfe5f055
RajanBakshi238 <rajan.bakshi238@gmail.com>
2023-03-26 18:03:42 +0530

    css fix

--- client/src/Pages/FeatureAssign/index.jsx
+++ client/src/Pages/FeatureAssign/index.jsx
@@ -1,16 +1,26 @@
 import React, { useEffect, useState } from "react";
-
 import DataTable from "react-data-table-component";
 
+import { useAuth } from "../../context/AuthContext/context";
+
 const FeatureAssign = () => {
+  const { authObj: {assignedFeatures} } = useAuth();
+  const [data, setData] = useState()
+
+    console.log(assignedFeatures, ">>>>>>>>assigned features ")
+
   const columns = [
     {
       name: "Serial No.",
       selector: (row) => row.serial,
     },
     {
-      name: "Feature Name",
-      selector: (row) => row.title,
+      name: "Name",
+      selector: (row) => row.name,
+    },
+    {
+      name: "Feature Check Name",
+      selector: (row) => row.featureName,
     },
     {
       name: "Assigned To",
@@ -19,19 +29,31 @@ const FeatureAssign = () => {
         <div className="flex gap-2">
           <div className="form-control">
             <label className="label cursor-pointer">
-              <input type="checkbox" className="checkbox checkbox-sm checkbox-primary" />
+              <input
+                type="checkbox"
+                defaultChecked={row.allowedTo.includes("user")}
+                className="checkbox checkbox-sm checkbox-primary"
+              />
               <span className="label-text pl-2">User</span>
             </label>
           </div>
           <div className="form-control">
             <label className="label cursor-pointer">
-              <input type="checkbox" className="checkbox checkbox-sm checkbox-primary" />
+              <input
+                type="checkbox"
+                defaultChecked={row.allowedTo.includes("hr")}
+                className="checkbox checkbox-sm checkbox-primary"
+              />
               <span className="label-text pl-2">HR</span>
             </label>
           </div>
           <div className="form-control">
             <label className="label cursor-pointer">
-              <input type="checkbox" className="checkbox checkbox-sm checkbox-primary" />
+              <input
+                type="checkbox"
+                className="checkbox checkbox-sm checkbox-primary"
+                defaultChecked={row.allowedTo.includes("pm")}
+              />
               <span className="label-text pl-2">PM</span>
             </label>
           </div>
@@ -40,26 +62,18 @@ const FeatureAssign = () => {
     },
   ];
 
-  const [data, setData] = useState([
-    {
-      id: 1,
-      title: "DashBoard",
-      year: "1988",
-    },
-    {
-      id: 2,
-      title: "Profile",
-      year: "1984",
-    },
-    {
-      id: 3,
-      title: "Transaction",
-      year: "1984",
-    },
-  ]);
+  
 
   useEffect(() => {
-    setData(data.map((photo, index) => ({ serial: index + 1, ...photo })));
+    let featureData = [], i=1;
+    for(let feature in assignedFeatures){
+        featureData.push({...assignedFeatures[feature], serial: i})
+        i++;
+    }
+
+    console.log(featureData, ">>>>>>>>>> for in loop")
+
+    setData(featureData);
   }, []);
 
   return (
--- server/models/assignedFeaturesModel.js
+++ server/models/assignedFeaturesModel.js
@@ -3,13 +3,17 @@ const mongoose = require("mongoose");
 const assignedFeatureSchema = new mongoose.Schema({
     featureName: {
         type: String,
-        required: [true, "Feature name is required"]
+        required: [true, "Feature check name is required."]
+    },
+    name: {
+        type: String,
+        required: [true, "Feature name is required."]
     },
     allowedTo: {
         type: [String],
         default: ['admin'],
         enum: ['user', 'admin', 'hr', 'pm'],
-        required: [true, "Atleast one role required"]
+        required: [true, "Atleast one role required."]
     },
 }, {
     timestamps:  true
