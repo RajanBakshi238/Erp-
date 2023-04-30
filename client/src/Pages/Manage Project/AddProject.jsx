ab72b673f806c921a35d999bb528999d219f4e45
RajanBakshi238 <rajan.bakshi238@gmail.com>
2023-04-30 12:13:28 +0530

    add project ui done

--- client/src/Pages/Manage Project/AddProject.jsx
+++ client/src/Pages/Manage Project/AddProject.jsx
@@ -14,17 +14,61 @@ const AddProject = () => {
               <div>
                 <input
                   type="text"
-                  placeholder="Task Name"
+                  placeholder="Title"
                   className="input input-bordered bg-white focus:outline-[#8231d3] focus:border-[#8231d3] hover:border-[#8231d3] w-full"
                 />
               </div>
               <div>
+                <select className="select select-bordered bg-white focus:outline-[#8231d3] focus:border-[#8231d3] hover:border-[#8231d3] w-full">
+                  <option selected disabled hidden>
+                    Priority
+                  </option>
+                  <option>Low</option>
+                  <option>Medium</option>
+                  <option>High</option>
+                </select>
+              </div>
+              <div>
                 <input
                   type="text"
-                  placeholder="Task Description"
+                  placeholder="Price"
                   className="input input-bordered bg-white focus:outline-[#8231d3] focus:border-[#8231d3] hover:border-[#8231d3] w-full"
                 />
               </div>
+              <div>
+                <select className="select select-bordered bg-white focus:outline-[#8231d3] focus:border-[#8231d3] hover:border-[#8231d3] w-full">
+                  <option selected disabled hidden>
+                    Team Member
+                  </option>
+                  <option>Rajan</option>
+                  <option>Varun</option>
+                  <option>Karan</option>
+                </select>
+              </div>
+              <div className="col-start-1 col-end-3">
+                <textarea
+                  className="textarea textarea-bordered bg-white focus:outline-[#8231d3] focus:border-[#8231d3] hover:border-[#8231d3] w-full"
+                  placeholder="Description"
+                ></textarea>
+              </div>
+              <div className="form-control w-full col-start-1 col-end-3">
+                <label className="label">
+                  <span className="label-text">Upload Project Photos</span>
+                  {/* <span className="label-text-alt">Alt label</span> */}
+                </label>
+                <input
+                  type="file"
+                  className="file-input file-input-bordered bg-white focus:outline-[#8231d3] focus:border-[#8231d3] hover:border-[#8231d3] w-full"
+                />
+              </div>
+            </div>
+            <div className="flex justify-end mt-5">
+              <button
+                type="submit"
+                className="normal-case bg-[#8231d3] hover:bg-[#a158e0] hover:border-[#a158e0] btn btn-primary px-6"
+              >
+                Add Project
+              </button>
             </div>
           </div>
         </div>
