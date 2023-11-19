8e9e629dfc54965c061effcc0a20893630e8af4b
RajanBakshi238 <rajan.bakshi238@gmail.com>
2023-11-19 12:30:47 +0530

    role based authentication on the basis of permission type

--- server/middlewares/Authorize.js
+++ server/middlewares/Authorize.js
@@ -36,6 +36,38 @@ exports.verifyRoles = (check_name) => {
   };
 };
 
+exports.verifyPerissionRoles = (check_name, permissionType) => {
+  return async (req, res, next) => {
+    const featureDetail = await AssignedFeature.findOne({ featureKey: check_name });
+    console.log(featureDetail, "featureDetials.......")
+
+    if (!featureDetail) {
+      // i.e open for all
+
+      return res.status(403).json({
+        status: 403,
+        error: "No record found for this feature in backend!",
+      });
+    }
+
+    console.log(req.user, ">>>>>>>>>>>USER ON REQUEST OBJECT");
+    if (!req?.user) {
+      return res.status(401).json({
+        status: "fail",
+        error: "User not login!",
+      });
+    }
+
+    if (!req.user.permissions?.[check_name].includes(permissionType)) {
+      return res.status(403).json({
+        status: 403,
+        error: "You don't have permission to perform this action",
+      });
+    }
+    next();
+  };
+};
+
 // exports.verifyRoles = (...allowedRoles) => {
 //     return async (req, res, next) => {
 //         console.log(req.user, '>>>>>>>>>>>USER ON REQUEST OBJECT')
